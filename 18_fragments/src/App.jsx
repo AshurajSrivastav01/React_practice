421eab660ef860d97089116fd487556266c0931f
Ashuraj Srivastav <ashuraj.srivastav01@gmail.com>
2025-08-26 06:58:07 +0530

    Map and logical Opreators complete!

--- 18_fragments/src/App.jsx
+++ 18_fragments/src/App.jsx
@@ -1,9 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import "./App.css"
 function App() {
+  let foodItems = ['Dal', 'Green Vegetable', 'Roti', 'Salad', 'Milk', 'Ghee'];
   return (
     <>
-      <h1>Fragments</h1>
+      <h1>Food Items:</h1>
+      {foodItems.length === 0 && <h2>I'm Hungry.</h2> }
+      <ul className="list-group">
+      {foodItems.map((item) => (
+        <li key={item} className="list-group-item">{item}</li>
+      ))}
+      </ul>
     </>
   )
 }
