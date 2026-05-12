1da806fb1283b5bb53d269404f65748bd2c6ea10
Ashu Raj Srivastav <ashuraj.srivastav01@gmail.com>
2026-05-12 08:07:45 +0530

    sfew

--- day02/src/components/Toggle.jsx
+++ day02/src/components/Toggle.jsx
@@ -9,7 +9,7 @@ export function Toggle(){
 
     return(
         <>
-            <h1>Toggle Name by using 'useState'</h1>
+            <h1>Toggle Name using 'useState'</h1>
             <h2>User Name: {displayName ? 'Ashuraj Srivastav' : 'No Name' }</h2>
             <button type="button" onClick={toggleName}>Change Name</button>
         </>
