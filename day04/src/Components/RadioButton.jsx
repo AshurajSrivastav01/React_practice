0228495f98c829a2455bf6639cd2a2ec60e2f4f8
Ashu Raj Srivastav <ashuraj.srivastav01@gmail.com>
2026-06-01 18:12:36 +0530

    learnd how to implement use state in radio button

--- day04/src/Components/RadioButton.jsx
+++ day04/src/Components/RadioButton.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 export function RadioButton(){
+    const [gender, setGender] = useState('');
+    const handleGender = (event) => {
+        setGender(event.target.value);
+    }
     return (
         <>
             <h2>Select Gender:</h2>
+            <input type="radio" id="male" value="male" name="gender" checked={gender === 'male'} onChange={handleGender}/>
+            <label htmlFor="male">Male</label>
+
+            <input checked={gender === 'female'} onChange={handleGender} type="radio" id="female" value="female" name="gender"/>
+            <label htmlFor="female">Female</label>
+            <br></br>
+
+            <h3>Selected Gender: {gender}</h3>
         </>
     );
 }
