d13df5a67b9a6e80f9dad4ce5b1e52bb1489447b
Ashu Raj Srivastav <ashuraj.srivastav01@gmail.com>
2026-06-08 08:11:37 +0530

    -> I have added the mark completion Button -> if the task was marked complete then the color will change and the button will disappear What we can do? we can: - Add Task - Complete Task - Remove Task

--- Todo_app/src/Components/TaskList.jsx
+++ Todo_app/src/Components/TaskList.jsx
@@ -1,5 +1,12 @@
-const TaskList = ({task, removeTask}) => {
-        
+import { useState } from 'react';
+
+const TaskList = ({ task, removeTask }) => {
+    const [markComplete, setMarkComplete] = useState(false);
+
+    const completeTask = () => {
+        setMarkComplete(true);
+    };
+
     return (
         <>
             <div style={{
@@ -11,17 +18,25 @@ const TaskList = ({task, removeTask}) => {
                     justifyContent: "space-between",
                     padding: "3px 9px",
                     fontWeight: "500",
-                    fontSize: "15px"
+                    fontSize: "15px",
+                    textDecoration: markComplete ? "line-through" : "",
+                    color: markComplete ? "green" : ""
             }}>
                 <p>{task}</p>
-                <span 
-                    style={{
-                        cursor: "pointer"
-                    }} 
-                    onClick={()=>removeTask(task)}
-                >
-                    X
-                </span>
+                {!markComplete && (
+                    <div style={{ display: "flex", gap: "10px" }}>
+                        <span
+                            style={{ cursor: "pointer" }}
+                            onClick={completeTask}
+                        >
+                            ✔
+                        </span>
+
+                        <span style={{ cursor: "pointer" }} onClick={()=>removeTask(task)} >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                        </span>
+                    </div>
+                )}
             </div>
         </>
     );
