63d2b83356c3b9e3288e523d534953ddad4c3b0b
Ashu Raj Srivastav <ashuraj.srivastav01@gmail.com>
2026-06-09 06:50:28 +0530

    I'm learning UseEffect Hook

--- day08/src/App.jsx
+++ day08/src/App.jsx
@@ -3,9 +3,17 @@ import { useState } from 'react';
 
 function App() {
 
+  const [counter, setCounter] = useState(0);
+  function callOnce(){
+    console.log('callOnce function called');
+  }
+
+  callOnce();
+
   return (
     <>
       <h1>useEffect Hook</h1>
+      <button onClick={ () => setCounter(counter + 1) }>Counter {counter}</button>
     </>
   )
 }
