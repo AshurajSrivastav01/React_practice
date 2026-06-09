964940f07e8de35cde7463d44b97a8aef41767f7
Ashu Raj Srivastav <ashuraj.srivastav01@gmail.com>
2026-06-09 07:01:03 +0530

    I have learned about basic in useEffect

--- day08/src/App.jsx
+++ day08/src/App.jsx
@@ -1,19 +1,28 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
   const [counter, setCounter] = useState(0);
+  const [data, setData] = useState(0);
+  
   function callOnce(){
     console.log('callOnce function called');
   }
 
-  callOnce();
+  useEffect(()=>{
+    callOnce();
+  },[counter]);
+
 
   return (
     <>
       <h1>useEffect Hook</h1>
-      <button onClick={ () => setCounter(counter + 1) }>Counter {counter}</button>
+      <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+        <button style={{ width: "fit-content" }} onClick={ () => setCounter(counter + 1) }>Counter {counter}</button>
+        <br />
+        <button style={{ width: "fit-content" }} onClick={ () => setData(data + 1) }>Data {data}</button>
+      </div>
     </>
   )
 }
